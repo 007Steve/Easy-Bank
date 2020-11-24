0c383d22d17a89c6799d7aeaa42441dc861c9628
007steve <stephen.plummer1.sp@gmail.com>
2020-11-24 00:55:45 -0500

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../styles/Nav.css"
+function Nav() {
+    return (
+        <div className="nav">
+            <img src="" alt=""/>
+            <ul>
+                <li></li>
+            </ul>
+            <button>Request Invite</button>
+        </div>
+    )
+}
+
+export default Nav
